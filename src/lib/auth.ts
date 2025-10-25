7e015d1a0f3950570179900b94290a4317ffd36b
Andrik-Singh <andriksingh78@gmail.com>
2025-10-25 20:41:47 +0545

    Added auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,17 +1,27 @@
 import { db } from "@/db"
 import { betterAuth } from "better-auth"
 import { drizzleAdapter } from "better-auth/adapters/drizzle"
+import { forgotPassword } from "./mail/forgotPassword"
+import * as schema from "../db/schema"
 export const auth=betterAuth({
     database:drizzleAdapter(db,{
+        schema:schema,
         provider:"pg"
     }),
     emailAndPassword:{
-        enabled:true
+        enabled:true,
+        sendResetPassword:async({url,user})=>{
+            await forgotPassword(user,url)
+        }
     },
     socialProviders:{
         google:{
             clientSecret:process.env.GOOGLE_CLIENT_SECRET,
             clientId:process.env.GOOGLE_CLIENT_ID!
+        },
+        github:{
+            clientId:process.env.GITHUB_CLIENT_ID!,
+            clientSecret:process.env.GITHUB_CLIENT_SECRET,
         }
     }
 })
--- src/lib/authClient.ts
+++ src/lib/authClient.ts
@@ -1,2 +1,22 @@
-import { createAuthClient } from "better-auth/react"
-export const authClient = createAuthClient()
+import { createAuthClient } from "better-auth/react";
+export const authClient = createAuthClient({
+  baseURL: "http://localhost:3000",
+});
+export const signInWithGoogle = async () => {
+  const { data, error } = await authClient.signIn.social({
+    provider: "google",
+  });
+  return {
+    data,
+    error,
+  };
+};
+export const signInWithGIthub = async () => {
+  const { data, error } = await authClient.signIn.social({
+    provider: "github",
+  });
+  return {
+    data,
+    error,
+  };
+};
